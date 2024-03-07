1674a4bd314666d71533cf3f0c8332b256d9c24d
Tommi <tommi.savolainen79@gmail.com>
2024-03-07 13:09:11 +0200

    poistettu kommentti

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,37 +1,4 @@
 
-// const Course = (props) => {
-//   return(
-//   <>
-//   <Header course={props.course} />
-//   <Content course={props.course} />
-//   </>
-//   )
-// }
-
-// const Header = (props) => {
-//   return(
-//   <>
-//   <h2>{props.course.name}</h2>
-//   </>
-//   )
-// }
-
-// const Content = (props) => {
-//   return(
-//     <>
-//     <Part parts={props.course.parts}/>
-//     </>
-//   )
-// }
-
-// const Part = (props) => {
-//   return(
-//     <>
-//     {props.parts.map(part =>(<p key={part.id}>{part.name} {part.exercises}</p>))}
-//     <p><b>total of {props.parts.reduce((total, item) => total + item.exercises,0)} exercises</b></p>
-//     </>
-// )
-// }
 import Course from './components/Course'
 const App = () => {
   const courses = [
